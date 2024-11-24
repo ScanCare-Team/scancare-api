64f7fc7b34dbd3e44a2333ccade58e265754c17d
Asrini Muhsin <asrinimuhsin@gmail.com>
2024-11-24 18:29:05 +0800

    mengubah

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Route untuk register
 router.post('/register', authController.register);
 
-// Route untuk login
 router.post('/login', authController.login);
 
-// Route untuk mendapatkan data pengguna
 router.get('/user/:id', authController.getUserData);
 
-// Route untuk logout
 router.post('/logout', authController.logout);
 
 module.exports = router;
